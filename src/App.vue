cbf7f26a14a55c6b72c13649f0d0211de2ca6399
marianaar94 <marianaar@garagecoders.net>
2023-01-05 16:33:26 -0600

    vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <img src="./assets/logo.svg" alt="Logo vue" width="125" height="125" />
+    <small> <h1>Hola Mundo desde Vue.js3</h1></small>
+
+    <hr />
+
+    <h2>Contador: {{ contador }}</h2>
+    <button @click="incrementar">+1</button>
+    <button @click="decrementar">-1</button>
+    <button @click="resetear">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+small {
+  color: #666;
+}
+h1 {
+  font-weight: bolder;
+}
+
+h2 {
+  font-size: 2.5rem;
+}
+
+button {
+  cursor: pointer;
+  font-size: 1.5rem;
+  margin: 0px 6px;
+}
+</style>
+<script>
+export default {
+  data: () => ({
+    contador: 10,
+  }),
+  methods: {
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      this.contador--;
+    },
+    resetear() {
+      this.contador = 10;
+    },
+  },
+};
+</script>
